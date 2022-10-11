7a530e667aff20283a9d221c0101fbf9ae2b72b6
Luiz Ricardo Brumati De Lima <luiz.1303@hotmail.com>
2022-10-11 18:42:09 -0300

    Iniciando o desenvolvimento do front

--- recipesWebsite/views.py
+++ recipesWebsite/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home (request):
-    return render(request, 'recipeswebsite/home.html')
+    return render(request, 'recipeswebsite/home.html', context={
+        'name': 'Luiz Ricardo',
+    })
